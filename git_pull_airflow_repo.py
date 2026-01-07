8a623319b0b6a28c7914bd336894fbedad24b651
eliseodifolco <eliseodifolco@gmail.com>
2026-01-07 17:32:40 +0100

    dag git pull test 1

---
+++ git_pull_airflow_repo.py
@@ -0,0 +1,12 @@
+from airflow.decorators import dag
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+@dag(schedule=None, start_date=datetime(2025, 1, 1), catchup=False)
+def git_pull_dag_repo():
+    BashOperator(
+        task_id="git_pull",
+        bash_command="git -C /home/adm_difolco_e/air_disk/airflow/dags pull",
+    )
+
+dag = git_pull_dag_repo()
