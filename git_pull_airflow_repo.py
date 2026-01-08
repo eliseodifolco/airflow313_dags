132317aea943e2552b2e5c7d88cb50b3ae5ec28e
eliseodifolco <eliseodifolco@gmail.com>
2026-01-08 09:36:28 +0100

    test txt entry

--- git_pull_airflow_repo.py
+++ git_pull_airflow_repo.py
@@ -2,6 +2,8 @@ from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+'''this is the case where I still need to set saved credentials for github'''
+
 @dag(schedule=None, start_date=datetime(2025, 1, 1), catchup=False)
 def git_pull_dag_repo():
     BashOperator(
