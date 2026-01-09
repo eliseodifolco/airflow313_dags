3c3a1da60a30b64c754136a19e87ab42c293b440
eliseodifolco <eliseodifolco@gmail.com>
2026-01-09 16:11:13 +0100

    01 manual adjustments days ago elimination

--- prod_dag01_manual_adjustments.py
+++ prod_dag01_manual_adjustments.py
@@ -1,6 +1,5 @@
 import json
 from airflow.decorators import dag, task 
-from airflow.utils.dates import days_ago
 from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
 import snowflake.connector
 import pandas as pd
@@ -15,21 +14,21 @@ def _import_script_from_json(script_name):
         return dict[script_name]
 
 
-@dag(schedule=None, start_date=days_ago(1),catchup=False)
+@dag(schedule=None, start_date=pendulum.datetime(2024, 1, 1, tz="UTC"), catchup=False)
 def a01_prod_dag_manual_adjustments():
 
-    adjustment_wal_state_log = SnowflakeOperator(task_id='n_wal_state_log_adjustment',
-        sql=['''UPDATE ia.audimex_source.wal_state_log
-                SET CHANGE_DATE = '2023-05-15'
-                WHERE id = 109612'''],
+    adjustment_wal_state_log = SnowflakeOperator(
+        task_id='n_wal_state_log_adjustment',
+        sql=['''
+            UPDATE ia.audimex_source.wal_state_log
+            SET CHANGE_DATE = '2023-05-15'
+            WHERE id = 109612
+        '''],
         snowflake_conn_id='Snowflake_Key_Pair_Connection',
         warehouse='IA',
         database='IA',
         role='IA_PIPE_ADMIN',
         schema='AUDIMEX_SOURCE',
-        authenticator=None,
-        session_parameters=None,
-        )    
+    )
 
 a01_prod_dag_manual_adjustments()
-    
